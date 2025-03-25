4507d1c261c848dc61e8d7940a43406a68f5d043
Franco, Walter <WALTER_FRANCO@homedepot.com>
2025-03-24 21:03:02 -0600

    config for deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,15 +11,19 @@ const config: Config = {
 
   // Set the production url of your site here
   //url: 'https://your-docusaurus-site.example.com',
-  url: 'http://localhost',
+  //url: 'http://localhost',
+  url: 'https://ElDrTocino.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  //baseUrl: '/',
+  baseUrl: '/furiku-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'furiku',
+  //organizationName: 'furiku',
+  organizationName: 'ElDrTocino',
   projectName: 'furiku-docs',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
